04789400b0e3eb139dfdad07f5f83b9e0bb7f509
unknown <beauagst@gmail.com>
2017-10-04 17:53:38 +0100

    Initial commit

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-       "linebreak-style": ["error", "windows"],
-       "indent": ["error", 4],
-       "no-console": "off",
-    },
-};
